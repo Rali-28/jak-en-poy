3253d07f223e42ff155989f752a5de3d9e0c1bad
Rali-28 <113418936+Rali-28@users.noreply.github.com>
2023-04-23 11:02:09 +0800

    Format function using Prettier

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-
 // Generates random value of Rock, Paper, or Scissor
 const getComputerChoice = () => {
+    // Generates a random number between 0 - 2
+    let x = Math.random() * 3;
+    // Rounds down to get a whole number
+    let randomNum = Math.floor(x);
 
-  // Generates a random number between 0 - 2
-  let x = Math.random() * 3
-  // Rounds down to get a whole number
-  let randomNum = (Math.floor(x))
-
-  // Returns a random value of Rock, Paper, or Scissor
-  switch (randomNum) {
-    case 0: return "Rock"
-    case 1: return "Paper"
-    case 2: return "Scissor"
-  }
-}
-
+    // Returns a random value of Rock, Paper, or Scissor
+    switch (randomNum) {
+        case 0:
+            return "Rock";
+        case 1:
+            return "Paper";
+        case 2:
+            return "Scissor";
+    }
+};
